44438ea8ae9a07e92c7271259a317079a3e422a3
Basab K. Maulik <bkm.hadoop@gmail.com>
2012-02-11 22:12:35 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmpleSap::Application.config.secret_token = '2b49fa10701ed94b27457df0d8be4c114087e679b4140520ec2077238027c45ded50c72c1ea85a71796edfb40e869fa2f682da56a023b8f0b3bfff19066e63e5'

c528b882dade92ca185323935e8b84237ca8307a
Basab K. Maulik <bkm.hadoop@gmail.com>
2012-03-04 11:20:26 -0800

    Completed User model by adding secure password using the Rails 3.2 feature 'has_secure_password'.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.2'
 gem 'jquery-rails'
 gem 'haml'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 group :development do
   gem 'rspec-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,30 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Basab K. Maulik", email: "bkm.hadoop@gmail.com") }
+  before { @user = User.new(name: "Basab K. Maulik",
+                            email: "bkm.hadoop@gmail.com",
+                            password: "test_password",
+                            password_confirmation: "test_password") }
   subject { @user }
 
   it { should respond_to :name }
   it { should respond_to :email }
+  it { should respond_to :password_digest }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+  it { should respond_to :authenticate }
+
   it { should be_valid }
 
   context "when name is not present" do
@@ -65,6 +74,36 @@ describe User do
       user_with_upcased_email.email = @user.email.upcase
       user_with_upcased_email.save
     end
+    it { should_not be_valid }
+  end
+
+  context "when password is not present" do
+    before { @user.password = @user.password_confirmation = "" }
+    it { should_not be_valid }
+  end
+
+  context "when password and password_confirmation do not match" do
+    before { @user.password_confirmation = "not_test_password" }
+    it { should_not be_valid }
+  end
+
+  describe "user authentication and the return value of the authenticate method" do
+    before { @user.save }
+    let(:user_find_by_email) { User.find_by_email(@user.email) }
+
+    context "with valid password" do
+      it { should == user_find_by_email.authenticate(@user.password) }
+    end
+
+    context "with invalid password" do
+      let(:user_for_invalid_password) { user_find_by_email.authenticate("invalid") }
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "with a password of less than 6 characters" do
+    before { @user.password = "a" * 5 }
     it { should_not be_valid }
   end
 end
